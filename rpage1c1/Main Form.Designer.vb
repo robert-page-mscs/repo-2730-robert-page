88e42f70b52a20f2f65bd1c99e97532da21cef87
Robert Page <robert.page@my.southeastmn.edu>
2016-08-30 19:45:24 -0500

    Added ex1C

---
+++ rpage1c1/Main Form.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.picCarnival = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picCarnival, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Segoe UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(83, 49)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(233, 40)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Summer Carnival"
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.Font = New System.Drawing.Font("Segoe UI Semibold", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMsg.ForeColor = System.Drawing.Color.Red
+        Me.lblMsg.Location = New System.Drawing.Point(11, 278)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(376, 32)
+        Me.lblMsg.TabIndex = 2
+        Me.lblMsg.Text = "Come join the fun at Valley Park!"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 3500
+        '
+        'picCarnival
+        '
+        Me.picCarnival.Image = Global.rpage1c1.My.Resources.Resources.Carnival
+        Me.picCarnival.Location = New System.Drawing.Point(101, 109)
+        Me.picCarnival.Name = "picCarnival"
+        Me.picCarnival.Size = New System.Drawing.Size(196, 142)
+        Me.picCarnival.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picCarnival.TabIndex = 0
+        Me.picCarnival.TabStop = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 6000
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 9000
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(399, 361)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.picCarnival)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.picCarnival, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picCarnival As PictureBox
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblMsg As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ rpage1c1/Star Wars.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmStarWars
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblDarth = New System.Windows.Forms.Label()
+        Me.lblTrooper = New System.Windows.Forms.Label()
+        Me.picTrooper = New System.Windows.Forms.PictureBox()
+        Me.picDarth = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picTrooper, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDarth, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblDarth
+        '
+        Me.lblDarth.AutoSize = True
+        Me.lblDarth.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDarth.Location = New System.Drawing.Point(86, 305)
+        Me.lblDarth.Name = "lblDarth"
+        Me.lblDarth.Size = New System.Drawing.Size(83, 37)
+        Me.lblDarth.TabIndex = 2
+        Me.lblDarth.Text = "Darth"
+        '
+        'lblTrooper
+        '
+        Me.lblTrooper.AutoSize = True
+        Me.lblTrooper.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTrooper.Location = New System.Drawing.Point(256, 305)
+        Me.lblTrooper.Name = "lblTrooper"
+        Me.lblTrooper.Size = New System.Drawing.Size(176, 37)
+        Me.lblTrooper.TabIndex = 3
+        Me.lblTrooper.Text = "Stormtrooper"
+        '
+        'picTrooper
+        '
+        Me.picTrooper.Image = Global.rpage1c1.My.Resources.Resources.Trooper
+        Me.picTrooper.Location = New System.Drawing.Point(244, 24)
+        Me.picTrooper.Name = "picTrooper"
+        Me.picTrooper.Size = New System.Drawing.Size(200, 250)
+        Me.picTrooper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picTrooper.TabIndex = 1
+        Me.picTrooper.TabStop = False
+        '
+        'picDarth
+        '
+        Me.picDarth.Image = Global.rpage1c1.My.Resources.Resources.Darth
+        Me.picDarth.Location = New System.Drawing.Point(27, 24)
+        Me.picDarth.Name = "picDarth"
+        Me.picDarth.Size = New System.Drawing.Size(200, 250)
+        Me.picDarth.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picDarth.TabIndex = 0
+        Me.picDarth.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 3000
+        '
+        'frmStarWars
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(466, 387)
+        Me.Controls.Add(Me.lblTrooper)
+        Me.Controls.Add(Me.lblDarth)
+        Me.Controls.Add(Me.picTrooper)
+        Me.Controls.Add(Me.picDarth)
+        Me.Name = "frmStarWars"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Star Wars Characters"
+        CType(Me.picTrooper, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDarth, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picDarth As PictureBox
+    Friend WithEvents picTrooper As PictureBox
+    Friend WithEvents lblDarth As Label
+    Friend WithEvents lblTrooper As Label
+    Friend WithEvents Timer1 As Timer
+End Class
