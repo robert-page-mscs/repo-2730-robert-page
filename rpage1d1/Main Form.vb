59bd97200ad683815d9fbb655b56a58b2ae603e7
Robert Page <robert.page@my.southeastmn.edu>
2016-09-01 20:47:34 -0500

    Added Ex1d: Bakery

---
+++ rpage1d1/Main Form.vb
@@ -0,0 +1,48 @@
+﻿' Name:           Bakery Project
+' Purpose:        Calculates the total number of items sold and total sales
+' Programmer:     Robert Page on 09/01/2016
+
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' prepare screen for next sale
+        txtDonuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+
+        ' send the focuse to the Doughnuts Box
+        txtDonuts.Focus()
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        ' print the sales reciept
+
+        PrintForm1.Print()
+
+        btnCalc.Visible = False
+        btnClear.Visible = False
+        btnExit.Visible = False
+        btnPrint.Visible = False
+
+        btnCalc.Visible = True
+        btnClear.Visible = True
+        btnExit.Visible = True
+        btnPrint.Visible = True
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' calculate numbert of items sold and total sales
+
+        lblTotalItems.Text = Val(txtDonuts.Text) + Val(txtMuffins.Text)
+        lblTotalSales.Text = Val(lblTotalItems.Text) * 0.5
+        lblTotalSales.Text = Format(lblTotalSales.Text, "currency")
+
+
+    End Sub
+End Class
